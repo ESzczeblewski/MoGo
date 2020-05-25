fff65b127aadf1188b561665c014e107b1b51620
Kilu691 <kilu691@gmail.com>
2020-05-25 19:33:39 +0200

    Finish mobile navigation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector('.navigation__hamburger');
+const hamburgerBox = document.querySelector('.navigation__hamburger__box');
+const navList = document.querySelector('.navigation__list');
+const drkBackground = document.querySelector('.dark-background');
 
 const handleClick = () => {
-  hamburger.classList.toggle('open')
+  hamburgerBox.classList.toggle('open');
+  navList.classList.toggle('show');
+  drkBackground.classList.toggle('dark-background--off');
 };
 
 hamburger.addEventListener('click', handleClick);
