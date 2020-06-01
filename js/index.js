6bc3f25a74bd1a9767d1a7a5af04bf23f0e6d9ae
Kilu691 <kilu691@gmail.com>
2020-06-01 18:36:34 +0200

    Final mobile main-content layout

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.navigation__hamburger');
 const hamburgerBox = document.querySelector('.navigation__hamburger__box');
 const navList = document.querySelector('.navigation__list');
 const drkBackground = document.querySelector('.dark-background');
+const mainImgsInfo = document.querySelectorAll('.main__imgs__pic__info');
 
 const handleClick = () => {
   hamburgerBox.classList.toggle('open');
@@ -10,3 +11,12 @@ const handleClick = () => {
 };
 
 hamburger.addEventListener('click', handleClick);
+
+document.querySelectorAll('.main__imgs__pic').forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.toggle('main__imgs--focus');
+    item.querySelectorAll('.main__imgs__pic__info').forEach(element => {
+      element.classList.toggle('show');
+    })
+  })
+})
