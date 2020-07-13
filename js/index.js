10403d031bf6917c11fedf6afa74423865139cf6
Kilu691 <kilu691@gmail.com>
2020-07-13 21:56:34 +0200

    Complete change of mobile nav

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
-const hamburger = document.querySelector('.navigation__hamburger');
-const hamburgerBox = document.querySelector('.navigation__hamburger__box');
-const navList = document.querySelector('.navigation__list');
-const drkBackground = document.querySelector('.dark-background');
+
 const mainImgsInfo = document.querySelectorAll('.main__imgs__pic__info');
 const weDoElement = document.querySelector('.we-do__info__block');
 const weDoArrow = document.querySelector('.we-do__info__block__arrow');
 const weDoDes = document.querySelector('.we-do__info__block__des');
 
-
-const handleClick = () => {
-  hamburgerBox.classList.toggle('open');
-  navList.classList.toggle('show');
-  drkBackground.classList.toggle('dark-background--off');
-};
-
-hamburger.addEventListener('click', handleClick);
 
 document.querySelectorAll('.main__imgs__pic').forEach(item => {
   item.addEventListener('click', event => {
